2561f24984d60fada20c40cd585bf43755cd7df2
kumanboy <husandavronov13@gmail.com>
2024-12-02 15:56:12 +0500

    Components end

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from "./components/Header.jsx";
+import MainContent from "./components/MainContent.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
     return (
         <div>
-
+            <Header/>
+            <MainContent/>
+            <Footer/>
         </div>
     )
 }
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MainContent = () => {
+    return (
+        <main>
+            <h2>Asosiy Content</h2>
+            <p>
+                fhjewhfuw fiwef uwie ufheiwfhufiw e
+            </p>
+        </main>
+    )
+}
+export default MainContent
