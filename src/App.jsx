2153ba36d667a47dcb7c46283cefc2bdb27558ce
kumanboy <husandavronov13@gmail.com>
2025-02-03 16:56:19 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
-import { useReducer } from 'react';
+import ShoppingCart from "./components/ShoppingCart.jsx";
 
-const initialState = { count: 0 };
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return { count: state.count + 1 };
-        case 'DECREMENT':
-            return { count: state.count - 1 };
-        case 'RESET':
-            return { count: 0 };
-        default:
-            throw new Error('Unknown action type');
-    }
-};
 export const App = () => {
-    const [state, dispatch] = useReducer(reducer, initialState );
+
 
     return (
-        <div>
-            <h1>Count: {state.count}</h1>
-            <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
-            <button onClick={() => dispatch({ type: 'DECREMENT' })}>Decrement</button>
-            <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
-        </div>
+        <ShoppingCart/>
     );
 };
 
